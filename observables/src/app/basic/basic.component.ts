e5e7f0f5009caf6f73cacca00e957732b45b0b00
ReginaldoPL <reginaldo.pedro.de.lima@gmail.com>
2020-02-25 22:53:07 -0300

    assincrona

--- observables/src/app/basic/basic.component.ts
+++ observables/src/app/basic/basic.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Observer } from 'rxjs';
-import { createOfflineCompileUrlResolver } from '@angular/compiler';
+import { Observable, Observer, Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-basic',
@@ -8,10 +8,19 @@ import { createOfflineCompileUrlResolver } from '@angular/compiler';
   styleUrls: ['./basic.component.css']
 })
 export class BasicComponent implements OnInit {
+  
+  subscription1: Subscription;
+  subscription2: Subscription;
+  n1 = 0;
+  n2 = 0;
+  s1 = ''
+  s2 = '';
 
   constructor() { }
 
   ngOnInit() {
+    this.s1 = 'Initialising ...';
+    this.s2 = 'Initialising ...';
     const myFirstObservable = new Observable(
       (obs: Observer<number>) => {
         obs.next(1);
@@ -27,8 +36,48 @@ export class BasicComponent implements OnInit {
       (n: number) => {console.log(n); } ,
       (error) => console.error(error),
       () => console.log('completed.')
-
     );
+    /*
+    const timerCount = interval(500);
+    timerCount.subscribe(
+      (n) => console.log(n)
+    )
+    console.log("after interval");
+    */
+
+    const myIntervalObservable = new Observable(
+      (observer: Observer<any>) => {
+        let i:number = 0;
+        let id = setInterval(() =>{
+          i++;
+          console.log('from Observable: ',i);
+          if (i == 10)
+            observer.complete();
+          else if (i%2 == 0)
+            observer.next(i);
+        }, 1000);
+        return () => {
+          clearInterval(id);
+        }
+      }
+    );
+    this.subscription1 = myIntervalObservable.subscribe(
+      (_n) => {this.n1 = _n},
+      (error) => {this.s1 = 'Error: '+error},
+      () => {this.s1 = "completed"}
+    );
+
+    this.subscription2 = myIntervalObservable.subscribe(
+      (_n) => {this.n2 = _n},
+      (error) => {this.s2 = 'Error: '+error},
+      () => {this.s1 = "completed"}
+    );
+    setTimeout(() =>{
+      this.subscription1.unsubscribe();
+      this.subscription2.unsubscribe();
+    }, 4000);
+
+
 
   }
 
