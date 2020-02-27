5c9a12a59767b3b1b0eb66e7626487feb808bd5d
ReginaldoPL <reginaldo.pedro.de.lima@gmail.com>
2020-02-26 23:19:09 -0300

    Hot Observavles criação

---
+++ observables/src/app/hot-observables/hot-observables.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hot-observables',
+  templateUrl: './hot-observables.component.html',
+  styleUrls: ['./hot-observables.component.css']
+})
+export class HotObservablesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
