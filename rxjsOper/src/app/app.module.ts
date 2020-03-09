50c7d67a8000714a19e989f6a1ceb2215d83e27a
ReginaldoPL <reginaldo.pedro.de.lima@gmail.com>
2020-03-09 18:07:14 -0300

    operador map e delay

--- rxjsOper/src/app/app.module.ts
+++ rxjsOper/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { BasicCreationComponent } from './basic-creation/basic-creation.component';
 import { FormsModule } from '@angular/forms';
+import { OperatorsComponent } from './operators/operators.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicCreationComponent
+    BasicCreationComponent,
+    OperatorsComponent
   ],
   imports: [
     BrowserModule,
