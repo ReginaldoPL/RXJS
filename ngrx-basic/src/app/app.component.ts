b3298bd2178cb8a74fcecfb5a4ed638757d50ba4
ReginaldoPL <reginaldo.pedro.de.lima@gmail.com>
2020-04-11 10:13:43 -0300

    criando projeto inicial NGRX, install ngRx e criar arquivo action

---
+++ ngrx-basic/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import {Person} from './person'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ 
+  people$ : Observable<Person[]>;
+  addNew(){
+
+  }
+
+  update(p:Person){
+
+  }
+
+  delete(p:Person){
+    
+  }
+}
---
+++ ngrx-basic/src/app/store/person.actions.ts
@@ -0,0 +1,35 @@
+import { Action } from '@ngrx/store';
+import { Person } from '../person';
+
+//intenção de fazer ações
+export enum PersonActionTypes {
+    PERSON_ALL = '[PERSON_ALL] Get All Person',
+    PERSON_NEW = '[PERSON_NEW] ADD a new Person',
+    PERSON_UPDATE = '[PERSON_UPDATE] UPDATE a Person',
+    PERSON_DELETE = '[PERSON_DELETE] DELETE a Person',
+}
+
+export class PersonAll implements Action {
+    readonly type = PersonActionTypes.PERSON_ALL;
+}
+
+export class PersonNew implements Action {
+    readonly type = PersonActionTypes.PERSON_NEW;
+    //melhor colocar sempre  payLoad: { person: Person }
+    constructor(public payLoad: { person: Person }) { }
+}
+
+export class PersonUpdate implements Action {
+    readonly type = PersonActionTypes.PERSON_UPDATE;
+    //melhor colocar sempre  payLoad: { person: Person }
+    constructor(public payLoad: { person: Person }) { }
+}
+
+export class PersonDelete implements Action {
+    readonly type = PersonActionTypes.PERSON_DELETE;
+    //melhor colocar sempre  payLoad: { id: String }
+    constructor(public payLoad: { id: String }) { }
+}
+
+//eportar tudo
+export type PersonActions = PersonAll | PersonNew | PersonUpdate | PersonDelete;
